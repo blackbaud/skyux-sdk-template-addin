7f888e890c44a7e276c6fde94363210818867aa0
Blackbaud-AdamHickey <Adam.Hickey@blackbaud.com>
2019-04-30 15:38:26 -0400

    Remove unnecessary modules (#3)
    Remove unnecessary modules

--- src/app/app-sky.module.ts
+++ src/app/app-sky.module.ts
@@ -1,23 +1,8 @@
-import {
-  NgModule
-} from '@angular/core';
-
-import {
-  SkyCheckboxModule
-} from '@skyux/forms';
-
-import {
-  SkyI18nModule
-} from '@skyux/i18n';
-
-import {
-  SkyAlertModule
-} from '@skyux/indicators';
+import { NgModule } from '@angular/core';
+import { SkyAlertModule } from '@skyux/indicators';
 
 @NgModule({
   exports: [
-    SkyCheckboxModule,
-    SkyI18nModule,
     SkyAlertModule
   ]
 })
