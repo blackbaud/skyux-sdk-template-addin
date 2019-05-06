ad824ac348d668684e34e3152998cca9d34a002c
Michael Tims <michaeltims81@gmail.com>
2019-05-06 15:59:22 -0400

    Changes for base addin template (#5)

--- src/app/my-tile/my-tile.component.ts
+++ src/app/my-tile/my-tile.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AddinClientService } from '@blackbaud/skyux-lib-addin-client';
-import { AddinClientInitArgs, AddinTileSummaryStyle } from '@blackbaud/sky-addin-client';
+import { AddinClientInitArgs } from '@blackbaud/sky-addin-client';
 
 @Component({
   selector: 'my-tile',
@@ -8,49 +8,17 @@ import { AddinClientInitArgs, AddinTileSummaryStyle } from '@blackbaud/sky-addin
   styleUrls: ['./my-tile.component.scss']
 })
 export class MyTileComponent implements OnInit {
-
-  public closeHelp: boolean = true;
-  public context: any;
-  public userIdentityToken: string;
-
   constructor(
     private addinClientService: AddinClientService
   ) {}
 
   public ngOnInit() {
     this.addinClientService.args.subscribe((args: AddinClientInitArgs) => {
-      this.context = args.context;
 
       args.ready({
         showUI: true,
-        title: 'My tile',
-        tileConfig: {
-          showHelp: true,
-          showSettings: true,
-          summaryStyle: AddinTileSummaryStyle.Check,
-          summaryChecked: true
-        }
+        title: 'My tile'
       });
     });
-
-    this.addinClientService.helpClick.subscribe(() => {
-      this.showHelp();
-    });
-  }
-
-  public helpClosed() {
-    this.closeHelp = true;
-  }
-
-  public getAuthToken() {
-    this.userIdentityToken = undefined;
-
-     this.addinClientService.getAuthToken().subscribe(token => {
-       this.userIdentityToken = token;
-     });
-  }
-
-  private showHelp() {
-    this.closeHelp = false;
   }
 }
